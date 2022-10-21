bb300559693212a0c9bb9264f7790bb789d0ba43
IrynaOkun <iryna.ladybug@gmail.com>
2022-10-21 06:21:31 +0300

    AlgoFindMinElInArray
    git commit -m AlgoFindMinElInArray

---
+++ src/AlgoFindMinElemInArrayr.java
@@ -0,0 +1,22 @@
+public class AlgoFindMinElemInArrayr {
+
+    public static void main(String[] args) {
+
+        int [] array = {56, 23, 89, 12, 45, -67, 23, 78, 23, 90, 43};
+        int minValue = array[0];
+        int minIndex = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+                minIndex = i;
+            }
+        }
+
+        System.out.println("min value: " + minValue);
+        System.out.println("min index: " + minIndex);
+
+
+
+    }
+}
