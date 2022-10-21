cfc5e984b97af3352d9e01246648a52181446c3c
IrynaOkun <iryna.ladybug@gmail.com>
2022-10-21 06:45:54 +0300

    plus text explanation

--- src/FindElementInArray.java
+++ src/FindElementInArray.java
@@ -14,12 +14,31 @@ public class FindElementInArray {
         return -1;
     }
 
+    //with text
+
+
+    public static int searchLinearText(int[] array, int elementToFind) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("searching at index: " + i + " , value: " + array[i]);
+            if (array[i] == elementToFind) {
+                System.out.println("found at index: "+ "\t" + i + " , value: " + array[i]);
+
+                return i;
+            }
+        }
+        System.out.println("not found: " + elementToFind);
+
+        return -1;
+    }
+
 
     public static void main(String[] args) {
 
         int[] array = new int[] {45, 11, 22, -56, 44, 23, -88, 45, 12, 34, 65, 89};
 
         System.out.println(searchLinear(array, 12));
+        System.out.println();
+        System.out.println(searchLinearText(array, 12));
     }
 
 }
